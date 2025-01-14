478e37494c521e5b0f5c6e66c38164893a71de65
noncolor76 <noncolor1976@gmail.com>
2025-01-14 16:25:23 +0900

    EP02 commit

---
+++ EP03_forMoon.py
@@ -0,0 +1,20 @@
+# range 함수 for 문
+# for num in range(1,101):
+#     print(num)
+#
+# print("end")
+
+# list for 문
+# fruits = ["사과","바나나","딸기"]
+#
+# for i in fruits:
+#     print(i)
+
+# dictionary for 문
+student_scores = {"철수":90,"영희":85,"민수":95}
+for name, score in student_scores.items():
+    print(name,score)
+
+
+
+
