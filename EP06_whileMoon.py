d7f219dfed94bad458a3d3326fe1d0c1af10d6c2
noncolor76 <noncolor1976@gmail.com>
2025-01-15 12:58:49 +0900

    EP02 commit

---
+++ EP06_whileMoon.py
@@ -0,0 +1,21 @@
+
+# range 함수 for 문
+# for num in range(1,101):
+#     print(num)
+#
+# print("end")
+
+num = 1
+
+while True:
+    print(num)
+    num = num + 1
+# while num <= 100
+    if num >= 101:
+        break
+
+
+print("end")
+
+
+
